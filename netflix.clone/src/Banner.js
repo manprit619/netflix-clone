24c8392e145bc7d80a6f5755ca369c7d27215d0e
manprit619 <manprit619@gmail.com>
2020-09-23 23:37:18 +0200

    first commit

---
+++ netflix.clone/src/Banner.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie); //to check on inspect
+
+  //frm sof
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+            "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+  )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        {/*title*/}
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button"> + My List</button>
+        </div>
+
+        <h1 className="banner_description">
+          {movie?.overview}
+
+          {truncate(movie?.overview, 15)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom"></div>
+      {/*div 2 buttons*/}
+      {/*description*/}
+    </header>
+  );
+}
+
+export default Banner;
+
+//div.banner_buttons>button.banner_buttons*2  shortcut command
+
+// download extension prettier, emmet, react snippets
